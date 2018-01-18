9a799196960075af2a797a45987027340cbdc977
JatinderJet <goldy.jatinder@gmail.com>
2018-01-18 13:22:10 +0530

    Cart Resource: cleaned

--- monolith-coolstore-app/src/main/java/com/redhat/coolstore/rest/CartResource.java
+++ monolith-coolstore-app/src/main/java/com/redhat/coolstore/rest/CartResource.java
@@ -1,10 +1,6 @@
 package com.redhat.coolstore.rest;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -14,158 +10,63 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javax.ws.rs.core.Response;
 
 import com.example.proprietary.customAnnotation.ProprietaryInitParam;
 import com.example.proprietary.customAnnotation.ProprietaryServlet;
-import com.redhat.coolstore.model.Product;
 import com.redhat.coolstore.model.ShoppingCart;
-import com.redhat.coolstore.model.ShoppingCartItem;
 import com.redhat.coolstore.service.cart.ShoppingCartService;
 
 @ProprietaryServlet(name="cart", runAs="SuperUser", initParams = { @ProprietaryInitParam (name="cart", value="spring") }, mapping = {"/cart/*"})
 @RequestScoped
 @Path("/cart")
 public class CartResource implements Serializable {
-	
-	private static final Logger LOG = LoggerFactory.getLogger(CartResource.class);
 
-    /**
-     *
-     */
-    private static final long serialVersionUID = -7227732980791688773L;
+	private static final long serialVersionUID = -7227732980791688773L;
 
-    @Inject
-    private ShoppingCartService shoppingCartService;
+	@Inject
+	private ShoppingCartService shoppingCartService;
 
-    @GET
-    @Path("/{cartId}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public ShoppingCart getCart(@PathParam("cartId") String cartId) {
+	@GET
+	@Path("/{cartId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public ShoppingCart getCart(@PathParam("cartId") String cartId) {
 
-        return shoppingCartService.getShoppingCart(cartId);
-    }
+		return shoppingCartService.getShoppingCart(cartId);
+	}
 
-    @POST
-    @Path("/{cartId}/{itemId}/{quantity}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public ShoppingCart add(@PathParam("cartId") String cartId,
-                            @PathParam("itemId") String itemId,
-                            @PathParam("quantity") int quantity) throws Exception {
-        ShoppingCart cart = shoppingCartService.getShoppingCart(cartId);
+	@POST
+	@Path("/{cartId}/{itemId}/{quantity}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public ShoppingCart add(@PathParam("cartId") String cartId,
+			@PathParam("itemId") String itemId,
+			@PathParam("quantity") int quantity) throws Exception {
+		try {
+			return shoppingCartService.addToCart(cartId, itemId, quantity);
+		} catch (Exception e) {
+			throw new WebApplicationException(Response.Status.SERVICE_UNAVAILABLE);
+		}
 
-        Product product = shoppingCartService.getProduct(itemId);
-        
-        if (product == null) {
-        	LOG.warn("Invalid product {} request to get added to the shopping cart. No product added", itemId);
-        	return cart;
-        }
+	}
 
-        ShoppingCartItem sci = new ShoppingCartItem();
-        sci.setProduct(product);
-        sci.setQuantity(quantity);
-        sci.setPrice(product.getPrice());
-        cart.addShoppingCartItem(sci);
+	@DELETE
+	@Path("/{cartId}/{itemId}/{quantity}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public ShoppingCart delete(@PathParam("cartId") String cartId,
+			@PathParam("itemId") String itemId,
+			@PathParam("quantity") int quantity) throws Exception {
+		return shoppingCartService.deleteItem(cartId, itemId, quantity);
+	}
 
-        try {
-            shoppingCartService.priceShoppingCart(cart);
-            cart.setShoppingCartItemList(dedupeCartItems(cart.getShoppingCartItemList()));
-        } catch (Exception ex) {
-            cart.removeShoppingCartItem(sci);
-            throw ex;
-        }
-
-        return cart;
-    }
-
-    @POST
-    @Path("/{cartId}/{tmpId}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public ShoppingCart set(@PathParam("cartId") String cartId,
-                            @PathParam("tmpId") String tmpId) throws Exception {
-
-        ShoppingCart cart = shoppingCartService.getShoppingCart(cartId);
-        ShoppingCart tmpCart = shoppingCartService.getShoppingCart(tmpId);
-
-        if (tmpCart != null) {
-            cart.resetShoppingCartItemList();
-            cart.setShoppingCartItemList(tmpCart.getShoppingCartItemList());
-        }
-
-        try {
-            shoppingCartService.priceShoppingCart(cart);
-            cart.setShoppingCartItemList(dedupeCartItems(cart.getShoppingCartItemList()));
-        } catch (Exception ex) {
-            throw ex;
-        }
-
-        return cart;
-    }
-
-    @DELETE
-    @Path("/{cartId}/{itemId}/{quantity}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public ShoppingCart delete(@PathParam("cartId") String cartId,
-                               @PathParam("itemId") String itemId,
-                               @PathParam("quantity") int quantity) throws Exception {
-    	
-    	return shoppingCartService.deleteItem(cartId, itemId, quantity);
-/*
-        List<ShoppingCartItem> toRemoveList = new ArrayList<ShoppingCartItem>();
-
-        ShoppingCart cart = shoppingCartService.getShoppingCart(cartId);
-
-        cart.getShoppingCartItemList().stream()
-                .filter(sci -> sci.getProduct().getItemId().equals(itemId))
-                .forEach(sci -> {
-                    if (quantity >= sci.getQuantity()) {
-                        toRemoveList.add(sci);
-                    } else {
-                        sci.setQuantity(sci.getQuantity() - quantity);
-                    }
-                });
-
-        toRemoveList.forEach(cart::removeShoppingCartItem);
-
-        shoppingCartService.priceShoppingCart(cart);
-         return cart;*/
-    }
-
-    @POST
-    @Path("/checkout/{cartId}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public ShoppingCart checkout(@PathParam("cartId") String cartId) {
-        // TODO: register purchase of shoppingCart items by specific user in session
-        ShoppingCart cart = shoppingCartService.getShoppingCart(cartId);
-        cart.resetShoppingCartItemList();
-        shoppingCartService.priceShoppingCart(cart);
-        return cart;
-    }
-
-    private List<ShoppingCartItem> dedupeCartItems(List<ShoppingCartItem> cartItems) {
-        List<ShoppingCartItem> result = new ArrayList<ShoppingCartItem>();
-        Map<String, Integer> quantityMap = new HashMap<String, Integer>();
-        for (ShoppingCartItem sci : cartItems) {
-            if (quantityMap.containsKey(sci.getProduct().getItemId())) {
-                quantityMap.put(sci.getProduct().getItemId(), quantityMap.get(sci.getProduct().getItemId()) + sci.getQuantity());
-            } else {
-                quantityMap.put(sci.getProduct().getItemId(), sci.getQuantity());
-            }
-        }
-
-        for (String itemId : quantityMap.keySet()) {
-            Product p = shoppingCartService.getProduct(itemId);
-            ShoppingCartItem newItem = new ShoppingCartItem();
-            newItem.setQuantity(quantityMap.get(itemId));
-            newItem.setPrice(p.getPrice());
-            newItem.setProduct(p);
-            result.add(newItem);
-        }
-        return result;
-    }
+	@POST
+	@Path("/checkout/{cartId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public ShoppingCart checkout(@PathParam("cartId") String cartId) {
+		ShoppingCart cart = shoppingCartService.checkout(cartId);
+		return cart;
+	}
 }
 
 
