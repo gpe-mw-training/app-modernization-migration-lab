62fae548b6a9276558f175c5c36bfad927ca1402
JatinderJet <goldy.jatinder@gmail.com>
2018-01-18 13:18:16 +0530

    removed the shipping service & promo service logic to PriceCalculation Service, using in-memory HashMap for cache serice thus removed infinispan dependency

--- monolith-coolstore-app/src/main/java/com/redhat/coolstore/service/cart/ShoppingCartService.java
+++ monolith-coolstore-app/src/main/java/com/redhat/coolstore/service/cart/ShoppingCartService.java
@@ -4,18 +4,12 @@ import com.redhat.coolstore.model.Product;
 import com.redhat.coolstore.model.ShoppingCart;
 import com.redhat.coolstore.model.ShoppingCartItem;
 import com.redhat.coolstore.service.catalog.CatalogService;
-
-import org.infinispan.client.hotrod.RemoteCacheManager;
-import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
-import org.infinispan.commons.api.BasicCacheContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,160 +20,65 @@ public class ShoppingCartService {
     private static final Logger LOG = LoggerFactory.getLogger(ShoppingCartService.class);
 
     @Inject
-    ShippingService ss;
-
-    @Inject
     CatalogService catalogServie;
 
     @Inject
-    PromoService ps;
+    private PriceCalculationService priceCalculationService;
 
-    Map<String, ShoppingCart> carts;
+    Map<String, ShoppingCart> carts=new HashMap<String, ShoppingCart>();;
 
     Map<String, Product> productMap = new HashMap<String, Product>();
 
-    @PostConstruct
-    public void init() {
-        String host = System.getenv("DATAGRID_HOST");
-        String port = System.getenv("DATAGRID_PORT");
-
-        if (host != null) {
-            try {
-                ConfigurationBuilder builder = new ConfigurationBuilder();
-                builder.addServer()
-                        .host(host)
-                        .port(Integer.parseInt(port));
-                BasicCacheContainer manager = new RemoteCacheManager(builder.build());
-                carts = manager.getCache("cart");
-
-                LOG.info("Using remote JBoss Data Grid (Hot Rod) on {}:{} for cart data", host, port);
-            } catch (Exception ex) {
-                LOG.error("Failed to connect to remote JBoss Data Grid (Hot Rod) on {}:{}", host, port);
-            }
-        }
-
-        if (carts == null) {
-            LOG.info("Using local cache for cart data");
-            carts = new HashMap<String, ShoppingCart>();
-        }
-    }
-
     public ShoppingCart getShoppingCart(String cartId) {
-        if (!carts.containsKey(cartId)) {
-            ShoppingCart cart = new ShoppingCart(cartId);
+    	ShoppingCart cart = carts.get(cartId);
+        if (cart == null) {
+            cart = new ShoppingCart(cartId);
+            cart.setCartId(cartId);
             carts.put(cartId, cart);
-            return cart;
         }
-
-        ShoppingCart cart = carts.get(cartId);
-        priceShoppingCart(cart);
-        carts.put(cartId, cart);
         return cart;
     }
 
-    public void priceShoppingCart(ShoppingCart sc) {
-        if (sc != null) {
-            initShoppingCartForPricing(sc);
-
-            if (sc.getShoppingCartItemList() != null && sc.getShoppingCartItemList().size() > 0) {
-                ps.applyCartItemPromotions(sc);
-
-                for (ShoppingCartItem sci : sc.getShoppingCartItemList()) {
-                    sc.setCartItemPromoSavings(sc.getCartItemPromoSavings() + sci.getPromoSavings() * sci.getQuantity());
-                    sc.setCartItemTotal(sc.getCartItemTotal() + sci.getPrice() * sci.getQuantity());
-                }
-
-                ss.calculateShipping(sc);
-            }
-
-            ps.applyShippingPromotions(sc);
-
-            sc.setCartTotal(sc.getCartItemTotal() + sc.getShippingTotal());
-        }
-    }
-
-    void initShoppingCartForPricing(ShoppingCart sc) {
-        sc.setCartItemTotal(0);
-        sc.setCartItemPromoSavings(0);
-        sc.setShippingTotal(0);
-        sc.setShippingPromoSavings(0);
-        sc.setCartTotal(0);
-
-        for (ShoppingCartItem sci : sc.getShoppingCartItemList()) {
-            Product p = getProduct(sci.getProduct().getItemId());
-
-            //if product exist, create new product to reset price
-            if (p != null) {
-                sci.setProduct(new Product(p.getItemId(), p.getName(), p.getDescription(), p.getPrice()));
-                sci.setPrice(p.getPrice());
-            }
-
-            sci.setPromoSavings(0);
-        }
-    }
 
     public Product getProduct(String itemId) {
-        if (!productMap.containsKey(itemId)) {
-            // Fetch and cache products. TODO: Cache should expire at some point!
-            List<Product> products = catalogServie.getProducts();
-            for (Product product : products) {
-            	productMap.put(product.getItemId(), product);
-			}
-//            productMap = products.stream().collect(Collectors.toMap(Product::getItemId, Function.identity()));
+    	Product product = productMap.get(itemId);
+        if (product == null) {
+            product = catalogServie.getProduct(itemId);
+            productMap.put(itemId, product);
         }
-
-        return productMap.get(itemId);
+        return product;
     }
 
     public ShoppingCart deleteItem(String cartId, String itemId, int quantity) {
-        List<ShoppingCartItem> toRemoveList = new ArrayList<ShoppingCartItem>();
+    	ShoppingCart sc = getShoppingCart(cartId); 
+    	if (quantity <= 0) {
+    		return sc;
+    	}
+    	List<ShoppingCartItem> listShoppingCartItems = sc.getShoppingCartItemList();
 
-        ShoppingCart cart = getShoppingCart(cartId);
-        
-        List<ShoppingCartItem> listShoppingCartItems = cart.getShoppingCartItemList();
-        
-       /* cart.getShoppingCartItemList().stream()
-                .filter(sci -> sci.getProduct().getItemId().equals(itemId))
-                .forEach(sci -> {
-                    if (quantity >= sci.getQuantity()) {
-                        toRemoveList.add(sci);
-                    } else {
-                        sci.setQuantity(sci.getQuantity() - quantity);
-                    }
-                });*/
-        for (ShoppingCartItem sci : listShoppingCartItems) {
-        	if(sci.getProduct().getItemId().equals(itemId)){
-        		if(quantity >= sci.getQuantity()){
-        			toRemoveList.add(sci);
-        		}else{
-        			sci.setQuantity(sci.getQuantity() - quantity);
-        		}
-        		
-        	}
-			
-		}
-        
-        
-        //toRemoveList.forEach(cart::removeShoppingCartItem);
-        for (ShoppingCartItem removeSci : toRemoveList) {
-        	cart.removeShoppingCartItem(removeSci);
-		}
-       
-        priceShoppingCart(cart);
-        carts.put(cartId, cart);
-
-        return cart;
+    	for (ShoppingCartItem cartItem : listShoppingCartItems) {
+    		if(cartItem.getProduct().getItemId().equals(itemId)){
+    			if(quantity >= cartItem.getQuantity()){
+    				sc.removeShoppingCartItem(cartItem);
+    			}else{
+    				cartItem.setQuantity(cartItem.getQuantity() - quantity);
+    			}
+    			calculateCartPrice(sc);
+    			carts.put(cartId, sc);
+    		}
+    	}
+    	return sc;    	
     }
 
     public ShoppingCart checkout(String cartId) {
-        ShoppingCart cart = getShoppingCart(cartId);
-        cart.resetShoppingCartItemList();
-        priceShoppingCart(cart);
-        carts.put(cartId, cart);
-        return cart;
+    	ShoppingCart sc = new ShoppingCart();
+        sc.setCartId(cartId);
+        carts.put(sc.getCartId(), sc);
+        return sc;
     }
 
-    public ShoppingCart addItem(String cartId, String itemId, int quantity) throws Exception {
+   
+    public ShoppingCart addToCart(String cartId, String itemId, int quantity) throws Exception {
         ShoppingCart cart = getShoppingCart(cartId);
         Product product = getProduct(itemId);
 
@@ -194,59 +93,15 @@ public class ShoppingCartService {
         sci.setPrice(product.getPrice());
         cart.addShoppingCartItem(sci);
 
-        try {
-            priceShoppingCart(cart);
-            cart.setShoppingCartItemList(dedupeCartItems(cart));
-        } catch (Exception ex) {
-            cart.removeShoppingCartItem(sci);
-            throw ex;
-        }
-
+        calculateCartPrice(cart);
         carts.put(cartId, cart);
         return cart;
+        
+        
     }
 
-    public ShoppingCart set(String cartId, String tmpId) throws Exception {
-
-        ShoppingCart cart = getShoppingCart(cartId);
-        ShoppingCart tmpCart = getShoppingCart(tmpId);
-
-        if (tmpCart != null) {
-            cart.resetShoppingCartItemList();
-            cart.setShoppingCartItemList(tmpCart.getShoppingCartItemList());
-        }
-
-        try {
-            priceShoppingCart(cart);
-            cart.setShoppingCartItemList(dedupeCartItems(cart));
-        } catch (Exception ex) {
-            throw ex;
-        }
-
-        carts.put(cartId, cart);
-        return cart;
-    }
-
-    List<ShoppingCartItem> dedupeCartItems(ShoppingCart sc) {
-        List<ShoppingCartItem> result = new ArrayList<ShoppingCartItem>();
-        Map<String, Integer> quantityMap = new HashMap<String, Integer>();
-        for (ShoppingCartItem sci : sc.getShoppingCartItemList()) {
-            if (quantityMap.containsKey(sci.getProduct().getItemId())) {
-                quantityMap.put(sci.getProduct().getItemId(), quantityMap.get(sci.getProduct().getItemId()) + sci.getQuantity());
-            } else {
-                quantityMap.put(sci.getProduct().getItemId(), sci.getQuantity());
-            }
-        }
-
-        for (String itemId : quantityMap.keySet()) {
-            Product p = getProduct(itemId);
-            ShoppingCartItem newItem = new ShoppingCartItem();
-            newItem.setQuantity(quantityMap.get(itemId));
-            newItem.setPrice(p.getPrice());
-            newItem.setProduct(p);
-            result.add(newItem);
-        }
-
-        return result;
+    public ShoppingCart calculateCartPrice(ShoppingCart sc) {
+        priceCalculationService.priceShoppingCart(sc);
+        return sc;
     }
 }
